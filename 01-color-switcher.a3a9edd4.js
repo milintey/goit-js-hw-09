34f2aa5bd6f88be9d019d624bcf097a342fed2aa
milintey <96431426+milintey@users.noreply.github.com>
2022-06-26 19:49:04 +0000

    Deploying to gh-pages from @ milintey/goit-js-hw-09@61e697f13a857374556923512f86baacef52ddbe 🚀

---
+++ 01-color-switcher.a3a9edd4.js
@@ -0,0 +1,2 @@
+document.querySelector("[data-start"),document.querySelector("[data-stop]");const e=document.querySelector("body");console.log(e);
+//# sourceMappingURL=01-color-switcher.a3a9edd4.js.map
---
+++ 01-color-switcher.f951f21e.js
@@ -0,0 +1,2 @@
+!function(){document.querySelector("[data-start"),document.querySelector("[data-stop]");var e=document.querySelector("body");console.log(e)}();
+//# sourceMappingURL=01-color-switcher.f951f21e.js.map
