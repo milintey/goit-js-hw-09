35e53100030aee8dd331b5022399b3b18b3f647d
Ilya Milinteev <ilyamilint@gmail.com>
2022-06-27 16:49:44 +0300

    comit 4

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,7 +1,31 @@
 const startBtn = document.querySelector('[data-start');
 const stopBtn = document.querySelector('[data-stop]');
 const body = document.querySelector('body');
-console.log(body);
+
+let intervalId = null;
+
+startBtn.addEventListener('click', onStartColor);
+stopBtn.addEventListener('click', onStopColor);
+
+
+function onStartColor() {
+  const colorBody = getRandomHexColor();
+  body.style.backgroundColor = colorBody;
+  
+  intervalId = setInterval(() => {
+    const colorBody = getRandomHexColor();
+    body.style.backgroundColor = colorBody;
+  }, 1000);
+
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+}
+
+function onStopColor() {
+  clearInterval(intervalId);
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+}
 
 
 function getRandomHexColor() {
